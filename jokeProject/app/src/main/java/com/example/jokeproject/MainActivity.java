64625bde1dafd39e31651828d7685aac6cec3135
Kendric Terry <kendric285@gmail.com>
2020-06-27 12:10:41 -0400

    k

--- jokeProject/app/src/main/java/com/example/jokeproject/MainActivity.java
+++ jokeProject/app/src/main/java/com/example/jokeproject/MainActivity.java
@@ -30,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
         programming = findViewById(R.id.programming);
         sports = findViewById(R.id.sports);
 
+        //change
+        
+
 
         random.setOnClickListener(new View.OnClickListener() {
             @Override
